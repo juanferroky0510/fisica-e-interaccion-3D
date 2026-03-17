d204657b595eb2f35ff23d5f57504be5029e3a32
juanferroky0510 <jfoo510roky@gmail.com>
2026-03-17 14:10:50 -0600

    se cambio escenario

--- assets/js/games_fps.js
+++ assets/js/games_fps.js
@@ -87,7 +87,7 @@ for (let i = 0; i < NUM_SPHERES; i++) {
 
 const worldOctree = new Octree();
 
-const playerCollider = new Capsule(new THREE.Vector3(0, 0.35, 0), new THREE.Vector3(0, 1, 0), 0.35);
+const playerCollider = new Capsule(new THREE.Vector3(0, 0.35, 0), new THREE.Vector3(0, 1.7, 0), 0.35);
 
 const playerVelocity = new THREE.Vector3();
 const playerDirection = new THREE.Vector3();
@@ -387,7 +387,7 @@ function controls(deltaTime) {
 
 const loader = new GLTFLoader().setPath('./models/gltf/');
 
-loader.load('collision-world.glb', (gltf) => {
+loader.load('scene.gltf', (gltf) => {
 
     scene.add(gltf.scene);
 
@@ -429,7 +429,7 @@ function teleportPlayerIfOob() {
     if (camera.position.y <= - 25) {
 
         playerCollider.start.set(0, 0.35, 0);
-        playerCollider.end.set(0, 1, 0);
+        playerCollider.end.set(0, 1.7, 0);
         playerCollider.radius = 0.35;
         camera.position.copy(playerCollider.end);
         camera.rotation.set(0, 0, 0);
